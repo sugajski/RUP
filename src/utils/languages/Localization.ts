58fdab2d40cb17f07acd869d8d832408a2031505
Maciej Sugajski <maciej.sugajski@BENEFIT1882.local>
2022-07-09 16:45:01 +0200

    Added english language support.

--- src/utils/languages/Localization.ts
+++ src/utils/languages/Localization.ts
@@ -5,10 +5,12 @@ import 'moment/min/locales.min';
 import get from 'lodash/get';
 
 import pl from './pl.json';
+import en from './en.json';
 
 const polishLanguageCode = 'pl';
+const englishLanguageCode = 'en';
 
-const supportedLanguages = [polishLanguageCode];
+const supportedLanguages = [polishLanguageCode, englishLanguageCode];
 
 const locales = RNLocalize.getLocales();
 const systemLanguageCode = get(locales, '0.languageCode', null);
@@ -21,7 +23,7 @@ I18n.locale = languageToSet;
 moment.locale(languageToSet);
 
 I18n.fallbacks = true;
-I18n.translations = { pl };
+I18n.translations = { pl, en };
 
 const Localization = I18n;
 
